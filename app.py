c7c73dad3360c80efa67851ff4e6dd3f200c3658
SrosasgoUNIN <srosasgo@uninpahu.edu.co>
2026-04-25 12:07:45 -0500

    commit 4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hola DevSecOps desde GitHub 🚀"
+    return "Hola DevSecOps desde GitHub hola 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)  
